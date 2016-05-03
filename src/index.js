f571167fd5ca2d68b907ae71130daf1ed0ba08cf
Gio d'Amelio <giodamelio@gmail.com>
2016-05-03 08:56:17 -0700

    Setup koa router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import Koa from 'koa';
+import Router from 'koa-router';
 
 const server = new Koa();
+const router = new Router();
 
-server.use(async (ctx) => {
+router.get('/', async (ctx) => {
   ctx.body = 'Hello World!';
 });
+
+// Add routes to the server
+server.use(router.routes());
+server.use(router.allowedMethods());
 
 export default server;
