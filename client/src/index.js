8fae10ccdead39ca611092164068beb8820e17b6
Gio d'Amelio <giodamelio@gmail.com>
2016-05-03 12:24:13 -0700

    React hello world

--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,13 @@
-console.log(`1 + 100 = ${1 + 100}`);
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Hello extends React.Component {
+  render() {
+    return <h1>Hello World!!!</h1>;
+  }
+}
+
+ReactDOM.render(
+  <Hello />,
+  document.getElementById('app')
+);
