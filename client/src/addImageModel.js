46f29f144f6586b05f03e53117ae74db695e4798
Gio d'Amelio <giodamelio@gmail.com>
2016-05-04 11:04:09 -0700

    Add new image to the state

--- client/src/addImageModel.js
+++ client/src/addImageModel.js
@@ -9,8 +9,17 @@ class AddImageModel extends React.Component {
     super();
 
     this.close = this.close.bind(this);
+    this.addImage = this.addImage.bind(this);
+    this.handleUrlChange = this.handleUrlChange.bind(this);
+    this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
+
+    this.state = {
+      url: '',
+      description: '',
+    };
   }
 
+  // Close the model
   close() {
     this.props.dispatch(
       actions.showAddImageModel,
@@ -18,17 +27,72 @@ class AddImageModel extends React.Component {
     );
   }
 
+  // Add an image to the gallery
+  addImage() {
+    this.close();
+
+    this.props.dispatch(
+      actions.addImage,
+      {
+        url: this.state.url,
+        description: this.state.description,
+      }
+    );
+
+    this.setState({
+      url: '',
+      description: '',
+    });
+  }
+
+  // Handle the url
+  handleUrlChange(event) {
+    this.setState({
+      url: event.target.value,
+    });
+  }
+
+  // Handle the description
+  handleDescriptionChange(event) {
+    this.setState({
+      description: event.target.value,
+    });
+  }
+
   render() {
     return (
       <Modal show={this.props.isAddingImage} onHide={this.close}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>Add Image to Gallery</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <h4>Text in a modal</h4>
+          <form>
+            <div className="form-group">
+              <label htmlFor="url">Image Url</label>
+              <input
+                type="url"
+                value={this.state.url}
+                onChange={this.handleUrlChange}
+                className="form-control"
+                id="url"
+                placeholder="Url"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="description">Description</label>
+              <input
+                type="text"
+                value={this.state.description}
+                onChange={this.handleDescriptionChange}
+                className="form-control"
+                id="description"
+                placeholder="Description"
+              />
+            </div>
+          </form>
         </Modal.Body>
         <Modal.Footer>
-          <Button onClick={this.close}>Close</Button>
+          <Button onClick={this.addImage}>Submit</Button>
         </Modal.Footer>
       </Modal>
     );
